d7b3608fc1e6d374cec01a8eab1b9c91673bb3d3
Wraith <1363380539@qq.com>
2022-01-12 13:55:09 +0800

    1/12 加入Arouter实现组件跳转

---
+++ app/src/main/java/com/example/basketball/MainApplication.java
@@ -0,0 +1,37 @@
+package com.example.basketball;
+
+import android.app.Application;
+
+import com.alibaba.android.arouter.launcher.ARouter;
+import com.example.commlib.BaseApplication;
+
+public class MainApplication extends BaseApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //ARouter后台有ILogger接口，定义了一些输出日志
+        if (isDebug()) {           // 这两行必须写在init之前，否则这些配置在init过程中将无效
+            ARouter.openLog();     // 打印日志
+            ARouter.openDebug();   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
+        }
+        ARouter.init(this); // 尽可能早，推荐在Application中初始化ARouter
+
+        init(this);
+        initover(this);
+    }
+
+    private boolean isDebug() {
+        return BuildConfig.DEBUG;
+    }
+
+
+    @Override
+    public void init(Application application) {
+
+    }
+
+    @Override
+    public void initover(Application application) {
+    }
+}
