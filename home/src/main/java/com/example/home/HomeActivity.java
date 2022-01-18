8f927142a5656d936ffd120becdbfcfff3502dc0
JudiciousX <1119833684@qq.com>
2022-01-18 23:55:45 +0800

    personal模块初始化

--- home/src/main/java/com/example/home/HomeActivity.java
+++ home/src/main/java/com/example/home/HomeActivity.java
@@ -4,9 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.alibaba.android.arouter.facade.annotation.Route;
 
-@Route(path = "/home/home")
 public class HomeActivity extends AppCompatActivity {
 
     @Override
